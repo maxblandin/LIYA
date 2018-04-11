edb58fa23309524a0a550f3abfbee041338af948
maxblandin <maxence.blandin@gmail.com>
2018-04-11 08:58:30 +0200

    Correction du jeux de test !

--- app/src/main/java/com/mgr/narratif/game/liya/vue/activity/ChoixAventureActivity.java
+++ app/src/main/java/com/mgr/narratif/game/liya/vue/activity/ChoixAventureActivity.java
@@ -4,8 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.mgr.narratif.game.liya.R;
+import com.mgr.narratif.game.liya.dao.AventureDAO;
+import com.mgr.narratif.game.liya.model.Aventure;
+import com.mgr.narratif.game.liya.service.AventureService;
 
 public class ChoixAventureActivity extends AppCompatActivity {
+
+    AventureService avService = new AventureService();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
